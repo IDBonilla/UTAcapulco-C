f6b1df67c3c916b522e452ecfc24a3b18c73b222
DiegoDG-01 <31582257+DiegoDG-01@users.noreply.github.com>
2017-09-15 23:49:49 -0500

    Add files via upload

---
+++ hola munedo 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+   //El operador cout no inserta saltos de línea al final de la salida.Una manera de imprimir dos líneas es utilizar el manipulador endl, el cual colocará un salto de línea.
+    
+    cout << "Hola mundo" << endl;
+    cout << "I love Programming";
+    
+    return 0;
+}
