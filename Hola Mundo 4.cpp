7c1208d5186f85926472591cf2680dce7b6a1fae
DiegoDG-01 <31582257+DiegoDG-01@users.noreply.github.com>
2017-09-16 00:20:00 -0500

    Update Hola Mundo 4.cpp

--- Hola Mundo 4.cpp
+++ Hola Mundo 4.cpp
@@ -7,7 +7,9 @@ int main()
    cout << "Acapulco \n";
    cout << "UTAcapulco";
     
-   // El carácter para insertar líneas nuevas, \n, puede ser utilizado como una alternativa para endl. La barra invertida (\) es referida como un carácter de escape, e identifica un carácter "especial
-    
+   // El carácter para insertar líneas nuevas, \n, puede ser utilizado como una alternativa para endl.
+   // La barra invertida (\) es referida como un carácter de escape, e identifica un carácter "especial".
+   // Si se coloca mas de una \n se insertaran lineas nuevas dependiendo de la cantidad utilizada.
+   
    return 0;
 }
